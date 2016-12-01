2d0280249bafecb3e8d698f06ffe3796c7343cbf
kenirwin <kirwin@wittenberg.edu>
2016-12-01 12:43:03 -0700

    allow user to enter game number

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style type="text/css">
 @import url("dice3.css");
-body { background-color: darkgreen; }
+body { background-color: darkgreen; color: #fff}
 .button {
  padding: 15px 32px;
   text-align: center;
@@ -22,7 +22,14 @@ width: 10em;
 </style>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
 <?php
-   $js = file_get_contents('http://kenirwin.net/projects/reroll/api.php?'.$_SERVER['QUERY_STRING']);
+if (isset($_REQUEST['seed'])) {
+  $seed = $_REQUEST['seed'];
+}
+else { 
+  $seed = rand(1,100);
+}
+//$js = file_get_contents('http://kenirwin.net/projects/reroll/api.php?'.$_SERVER['QUERY_STRING']);
+$js = file_get_contents('http://kenirwin.net/projects/reroll/api.php?seed='.$seed);
 ?>
 <script>
 $(document).ready(function() {
@@ -40,7 +47,10 @@ $(document).ready(function() {
   });
 </script>
 <body>
-<center><div id="roll" class="button">Roll</div>
+<center>
+<h1>Game No. <?=$seed;?></h1>
+  <form>Set Game Number (1-100): <input type="text" size="5" name="seed"> <input type="submit" value="Enter"></form>
+<div id="roll" class="button">Roll</div>
 <div id="result"><div id="first"></div> <div id="second"></div></div>
 </center>
 </body>
